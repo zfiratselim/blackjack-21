60a3370ea1c299c6b467a1ae4ca7d94ceb7a0a6f
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2021-08-27 01:48:20 +0300

    added card info

---
+++ ts/config.ts
@@ -0,0 +1,110 @@
+export const cardSizes = {
+  width: 180,
+  height: 308
+}
+
+export const cardsElementsPosiiton = {
+  "1": [
+    { x: cardSizes.width / 2, y: cardSizes.height / 2 }
+  ],
+  "2": [
+    { x: cardSizes.width / 2, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 4 * 3 }
+  ],
+  "3": [
+    { x: cardSizes.width / 2, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 2 }
+  ],
+  "4": [
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 * 3 },
+  ],
+  "5": [
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 2 },
+  ],
+  "6": [
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 2 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 2 },
+  ],
+  "7": [
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 2 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 2 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 8 * 3 },
+  ],
+  "8": [
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 4 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 2 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 2 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 8 * 3 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 8 * 5 },
+  ],
+  "9": [
+    { x: cardSizes.width / 4, y: cardSizes.height / 5 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 5 * 4 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 5 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 5 * 4 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 5 * 2 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 5 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 5 * 2 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 5 * 3 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 2 }
+  ],
+  "10": [
+    { x: cardSizes.width / 4, y: cardSizes.height / 5 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 5 * 4 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 5 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 5 * 4 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 5 * 2 },
+    { x: cardSizes.width / 4, y: cardSizes.height / 5 * 3 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 5 * 2 },
+    { x: cardSizes.width / 4 * 3, y: cardSizes.height / 5 * 3 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 10 * 3 },
+    { x: cardSizes.width / 2, y: cardSizes.height / 10 * 7 },
+  ],
+  "J": {
+    logo: {
+      width: 100,
+      height: 75,
+      x: cardSizes.width / 2-65,
+      y: cardSizes.height / 2 - 10
+    },
+    hat: {
+      width: 160,
+      height: 82,
+      x: cardSizes.width / 2-70,
+      y: cardSizes.height / 2 - 75
+    }
+  },
+  "Q": {
+    logo: {
+      width: 100,
+      height: 75,
+      x: cardSizes.width / 2-65,
+      y: cardSizes.height / 2 - 10
+    },
+    hat: {
+      width: 160,
+      height: 82,
+      x: cardSizes.width / 2-70,
+      y: cardSizes.height / 2 - 75
+    }
+  }
+}
