e1137486169bd2b3aa543e8ff6fd22b759659d03
Fırat Selim ZİREKOĞLU <zirekoglufs@gmail.com>
2021-09-24 12:35:12 +0300

    added betSlider methods

--- ts/betSlider.ts
+++ ts/betSlider.ts
@@ -1,15 +1,54 @@
 import * as PIXI from "pixi.js";
 import { SmoothGraphics as Graphics } from "@pixi/graphics-smooth";
+import { H, W } from "./config";
+
+const sliderSizes = {
+  width: 160,
+  height: 320
+}
 
 
 export default class BetSlider {
   private renderer;
+  private stage;
+  private betSliderCon: PIXI.Container = new PIXI.Container;
   constructor(stage, renderer) {
-    this.renderer = renderer
+    this.renderer = renderer;
+    this.stage = stage;
   }
-
+  addLayer() {
+    this.betSliderCon.x = W - 420;
+    this.betSliderCon.y = H - 430
+    this.stage.addChild(this.betSliderCon);
+  }
+  private addReactange({ width, height }: { width: number, height: number }, radius: number, color: number) {
+    let rectangle = new Graphics();
+    rectangle.beginFill(color);
+    rectangle.drawRoundedRect(0, 0, width, height, radius);
+    rectangle.endFill();
+    return rectangle;
+  }
+  addTireler() {
+    for (let i = 0; i < 10; i++) {
+      const tire = this.addReactange({ width: 45 - i * 2, height: 5 }, 5, 0xdddddd);
+      tire.position.set(58 + i, 30 * i + 20);
+      this.betSliderCon.addChild(tire);
+    }
+  }
+  addBg() {
+    const sliderBG = PIXI.Sprite.from("betSlider");
+    Object.assign(sliderBG, sliderSizes);
+    this.betSliderCon.addChild(sliderBG);
+  }
+  addSliderButton() {
+    const sliderScrollButton = PIXI.Sprite.from("sliderScrollButton");
+    sliderScrollButton.position.set(48, 265)
+    this.betSliderCon.addChild(sliderScrollButton);
+  }
   add() {
-
+    this.addBg();
+    this.addTireler();
+    this.addSliderButton();
   }
 
 }
--- ts/game.ts
+++ ts/game.ts
@@ -1,49 +1,21 @@
 import * as PIXI from "pixi.js";
-import { SmoothGraphics as Graphics } from "@pixi/graphics-smooth";
+
+import Table from "./table";
 import CardLayer from "./cardLayer";
 import ButtonLayer from "./buttonLayer";
-import { W, H, cardConCoords, cardConSizesForTable } from "./config";
+import BetSlider from "./betSlider";
+import { W, H } from "./config";
 import { Owner } from "./interface";
 
 
-class Table {
-  private stage
-  constructor(stage) {
-    this.stage = stage;
-  }
-  private addReactange(rot: number) {
-    let rectangle = new Graphics();
-    rectangle.lineStyle(2, 0xFFFFFF, 1);
-    rectangle.drawRoundedRect(0, 0, cardConSizesForTable.width, cardConSizesForTable.height, 5);
-    rectangle.endFill();
-    rectangle.rotation = rot;
-    return rectangle;
-  }
-  private addCardCons(BGCon: PIXI.Container) {
-    cardConCoords.forEach(e => {
-      const cardConReact = this.addReactange(e.rotation);
-      cardConReact.position.set(e.coords[0].x, e.coords[0].y);
-      BGCon.addChild(cardConReact)
-    })
-  }
-  add() {
-    const BGCon = new PIXI.Container();
-    const BG = PIXI.Sprite.from("bjTable");
-    BG.width = 1317;
-    BG.height = 740;
-    BG.position.set((W - 1317) / 2, (H - 740) / 2);
-    BGCon.position.set(0, 0);
-    BGCon.addChild(BG);
-    this.addCardCons(BGCon)
-    this.stage.addChild(BGCon);
-  }
-}
 
 
 export default class BlackJack extends PIXI.Application {
-  Table = new Table(this.stage);
-  CardLayer = new CardLayer(this.stage, this.renderer);
+  private Table = new Table(this.stage);
+  private CardLayer = new CardLayer(this.stage, this.renderer);
   private ButtonLayer = new ButtonLayer(this.stage, this.renderer);
+  private BetSlider = new BetSlider(this.stage, this.renderer);
+
   constructor(s) {
     super({
       view: <HTMLCanvasElement>document.querySelector("#canvas"),
@@ -66,6 +38,9 @@ export default class BlackJack extends PIXI.Application {
       .add("bjTable", "images/bj_table.png")
       .add("cardAltlik", "images/card_altlik.png")
       .add("cardUstluk", "images/card_ustluk.png")
+      .add("betSlider", "images/slider.png")
+      .add("sliderScrollButton", "images/slider_scroll_button.png")
+      .add("sliderButton", "images/slider_button.png")
       .load(() => this.startGame())
   }
 
@@ -84,9 +59,10 @@ export default class BlackJack extends PIXI.Application {
     })
   }
   startGame() {
-    this.Table.add();
     this.ticcker();
+    this.Table.add();
     this.CardLayer.addLayers();
+    this.BetSlider.addLayer();
     const showCards = (owner, i, A) => {
       for (let a = 0; a < A; a++) {
         setTimeout(() => {
@@ -96,8 +72,8 @@ export default class BlackJack extends PIXI.Application {
     }
     showCards(Owner.player1, 1, 5);
     showCards(Owner.player1, 2, 5);
-  
-    this.ButtonLayer.addButtons(0);
+    this.ButtonLayer.addButtons(1);
+    this.BetSlider.add();
   }
 }
 
